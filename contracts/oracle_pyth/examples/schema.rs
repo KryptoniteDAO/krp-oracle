b47da2d9f7d0a602f5a07d0507dfa67a6d9bc5bc
carson.xyg <carson.xyg@gmail.com>
2023-11-03 16:09:22 +0800

    update schema

--- contracts/oracle_pyth/examples/schema.rs
+++ contracts/oracle_pyth/examples/schema.rs
@@ -1,25 +1,12 @@
-use std::env::current_dir;
-use std::fs::create_dir_all;
+use cosmwasm_schema::write_api;
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use oracle_pyth::msg::{
-    ChangeOwnerMsg, ConfigResponse, ExecuteMsg, InstantiateMsg, PriceResponse,
-    PythFeederConfigResponse, QueryMsg, SetConfigFeedValidMsg,
-};
+use oracle_pyth::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
-    let mut out_dir = current_dir().unwrap();
-    out_dir.push("schema");
-    create_dir_all(&out_dir).unwrap();
-    remove_schemas(&out_dir).unwrap();
-
-    export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(ExecuteMsg), &out_dir);
-    export_schema(&schema_for!(SetConfigFeedValidMsg), &out_dir);
-    export_schema(&schema_for!(SetConfigFeedValidMsg), &out_dir);
-    export_schema(&schema_for!(ChangeOwnerMsg), &out_dir);
-    export_schema(&schema_for!(PriceResponse), &out_dir);
-    export_schema(&schema_for!(PythFeederConfigResponse), &out_dir);
-    export_schema(&schema_for!(ConfigResponse), &out_dir);
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+        // migrate: MigrateMsg
+    }
 }
